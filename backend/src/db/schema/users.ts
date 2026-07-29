e2d354200db501bcab3e1bfd5eef5fd3122bcdc7
MartaPagliuso <marta.pagliuso@tutamail.com>
2026-07-29 10:23:52 +0200

    Definita la tabella degli utenti

---
+++ backend/src/db/schema/users.ts
@@ -0,0 +1,16 @@
+import { pgEnum, pgTable as table, uuid, boolean, timestamp, text } from "drizzle-orm/pg-core";
+
+export const userRole = pgEnum('roles', ['user', 'admin']);
+
+export const users = table('users', {
+  id: uuid('id').defaultRandom().primaryKey(),
+  email: text('email').notNull(),
+  password: text('password').notNull(),
+  role: userRole('role').notNull().default('user'),
+  mustChangePassword: boolean('must_change_password').notNull().default(false),
+  createdAt: timestamp('created_at', { withTimezone: true }).notNull().defaultNow(),
+  updatedAt: timestamp('updated_at', { withTimezone: true }).notNull().defaultNow()
+});
+
+
+
