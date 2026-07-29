f37f3e29fe19941aeae092d4625da98f8805bdc4
MartaPagliuso <marta.pagliuso@tutamail.com>
2026-07-29 16:47:10 +0200

    Aggiunto il router per il login/logout in index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
 import express, { type Express, type Request, type Response } from 'express';
 import { userRouter } from './routes/users.route.js';
+import { authRouter } from './routes/auth.route.js';
 import { ensureDefaultAdmin } from './db/seed-admin.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
@@ -13,6 +14,7 @@ app.use(cookieParser());
 
 // rotte
 app.use('/users', userRouter);
+app.use('auth', authRouter);
 
 async function bootstrap() {
   await ensureDefaultAdmin();
